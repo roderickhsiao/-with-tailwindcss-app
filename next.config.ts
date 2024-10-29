a881eee8eb35b3c6abd2a9be20c9d04a87bbfce2
Roderick Hsiao <roderickhsiao@gmail.com>
2024-10-29 13:04:05 -0700

    add critters

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  experimental: {
+    optimizeCss: true,
+  },
   /* config options here */
 };
 
